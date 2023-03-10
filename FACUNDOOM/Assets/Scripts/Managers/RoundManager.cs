1052dbb3df3ab403b732511603a5cf0f9adfae9c
Esteban <estrestrepo+github@gmail.com>
2023-03-10 20:47:29 +0100

    Update RoundManager.cs

--- FACUNDOOM/Assets/Scripts/Managers/RoundManager.cs
+++ FACUNDOOM/Assets/Scripts/Managers/RoundManager.cs
@@ -1,33 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RoundManager : MonoBehaviour
 {
-    float m_timeSurvived = 0.0f;
-    bool m_bGameStarted = false;
-    //Aqui las pools de enemigos
+	public static RoundManager instance { get; private set; }
 
+	float m_timeSurvived = 0.0f;
+	bool m_bGameStarted = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public UnityEvent eEnemyDied;
+	public UnityEvent<Enemy> eWrongAnswer;
+	public UnityEvent<Vector3> eMissedAttack;
+	public UnityEvent eRandomizeColors;
 
-    // Update is called once per frame
-    void Update()
-    {
+	[SerializeField]
+	PlayerController m_player;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			Debug.Log("GameManager instanced");
+			instance = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if(eEnemyDied == null)
+		{
+			eEnemyDied = new UnityEvent();
+		}
+		if (eWrongAnswer == null)
+		{
+			eWrongAnswer = new UnityEvent<Enemy>();
+		}
+		if (eRandomizeColors == null)
+		{
+			eRandomizeColors = new UnityEvent();
+		}
+		if(eMissedAttack == null)
+		{
+			eMissedAttack = new UnityEvent<Vector3>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 		if (m_bGameStarted)
 		{
-            m_timeSurvived += Time.deltaTime;
+			m_timeSurvived += Time.deltaTime;
 		}
 
-        //If playerDead enseñar la UI de muerto y parar las entidades
-    }
+		//If playerDead enseñar la UI de muerto y parar las entidades
+	}
 
-    public void StartRound()
+	public void StartRound()
 	{
 
+	}
+
+	public PlayerController getPlayer()
+	{
+		return m_player;
 	}
 }
