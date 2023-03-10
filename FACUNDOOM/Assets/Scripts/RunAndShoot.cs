57fdc1480075631ecaacfd60ff227ac6e83e4108
David Godoy <dagodoy@ucm.es>
2023-03-10 16:18:51 +0100

    enemigo que dispara

--- FACUNDOOM/Assets/Scripts/RunAndShoot.cs
+++ FACUNDOOM/Assets/Scripts/RunAndShoot.cs
@@ -15,11 +15,15 @@ public class RunAndShoot : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed = 2;
+    [SerializeField]
+    private float projectileSpeed = 3;
 
     private float startRunTime = 0, startShootTime = 0;
 
     //Quitar que esto sea public en cuanto el GameManager tenga un getPlayer
     public Transform player;
+
+    public Transform projectilePrefab;
 
     private Rigidbody rb;
     // Start is called before the first frame update
@@ -87,6 +91,14 @@ public class RunAndShoot : MonoBehaviour
     {
         rb.velocity = Vector3.zero;
         //Instanciar proyectil
+        //Esto se deberia mover al proyectil cuando este el gamemanager bien
+        Rigidbody projectile = Instantiate(projectilePrefab).GetComponent<Rigidbody>();
+        projectile.gameObject.SetActive(true);
+        Vector3 movement = new Vector3(player.position.x - transform.position.x, 0f, player.position.z - transform.position.z);
+        projectile.transform.position = transform.position + movement.normalized;
+
+        projectile.velocity = movement.normalized * projectileSpeed;
+
         Debug.Log("DISPARO");
     }
 }
