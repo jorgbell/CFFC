b94a8559a0fbd051b9e3a7ef20ded7694989298b
David Godoy <dagodoy@ucm.es>
2023-03-11 19:08:49 +0100

    perro salto

--- FACUNDOOM/Assets/Scripts/RushAndJump.cs
+++ FACUNDOOM/Assets/Scripts/RushAndJump.cs
@@ -38,6 +38,7 @@ public class RushAndJump : MonoBehaviour
     void Update()
     {
         Vector3 toPlayer = new Vector3(player.position.x - transform.position.x, 0f, player.position.z - transform.position.z);
+        rb.rotation = Quaternion.LookRotation(toPlayer, Vector3.up);
         switch (currentState)
         {
             case (State.RUNNING):
@@ -83,9 +84,10 @@ public class RushAndJump : MonoBehaviour
 
     void Run()
     {
+        Debug.Log(rb.rotation);
         //transform.LookAt(target, Vector3.up);
         Vector3 movement = new Vector3(player.position.x - transform.position.x, 0f, player.position.z - transform.position.z);
-        rb.rotation = Quaternion.LookRotation(movement, Vector3.up);
+        //rb.rotation = Quaternion.LookRotation(movement, Vector3.up);
         //movement = Quaternion.FromToRotation(Vector3.forward, transform.forward) * movement.normalized * moveSpeed;
 
         movement = movement.normalized * moveSpeed;
